55473aa958e383545cfd5529f96bf412dd21f0d1
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-07-04 16:26:25 -0400

    particle

--- main.js
+++ main.js
@@ -106,6 +106,19 @@ function calculateDistance(aX, aY, bX, bY) {
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
+// Cleans up the canvas by removing older trails.
+function cleanCanvas() {
+    // Set 'destination-out' composite mode, so additional fill doesn't remove non-overlapping content.
+    context.globalCompositeOperation = 'destination-out';
+    // Set alpha level of content to remove.
+    // Lower value means trails remain on screen longer.
+    context.fillStyle = `rgba(0, 0, 0, ${CANVAS_CLEANUP_ALPHA})`;
+    // Fill entire canvas.
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // Reset composite mode to 'lighter', so overlapping particles brighten each other.
+    context.globalCompositeOperation = 'lighter';
+}
+
 // === END HELPERS ===
 
 // === EVENT LISTENERS ===
@@ -234,3 +247,77 @@ Firework.prototype.draw = function() {
         context.stroke();
     }
 }
+
+// Creates a new particle at provided 'x' and 'y' coordinates.
+function Particle(x, y) {
+    // Set current position.
+    this.x = x;
+    this.y = y;
+    // To better simulate a firework, set the angle of travel to random value in any direction.
+    this.angle = random(0, Math.PI * 2);
+    // Set friction.
+    this.friction = PARTICLE_FRICTION;
+    // Set gravity.
+    this.gravity = PARTICLE_GRAVITY;
+    // Set the hue to somewhat randomized number.
+    // This gives the particles within a firework explosion an appealing variance.
+    this.hue = random(hue - PARTICLE_HUE_VARIANCE, hue + PARTICLE_HUE_VARIANCE);
+    // Set brightness.
+    this.brightness = random(PARTICLE_BRIGHTNESS_MIN, PARTICLE_BRIGHTNESS_MAX);
+    // Set decay.
+    this.decay = random(PARTICLE_DECAY_MIN, PARTICLE_DECAY_MAX);
+    // Set speed.
+    this.speed = random(PARTICLE_SPEED_MIN, PARTICLE_SPEED_MAX);
+    // Create an array to track current trail particles.
+    this.trail = [];
+    // Trail length determines how many trailing particles are active at once.
+    this.trailLength = PARTICLE_TRAIL_LENGTH;
+    // While the trail length remains, add current point to trail list.
+    while(this.trailLength--) {
+        this.trail.push([this.x, this.y]);
+    }
+    // Set transparency.
+    this.transparency = PARTICLE_TRANSPARENCY;
+}
+
+// Update a particle prototype.
+// 'index' parameter is index in 'particles' array to remove, if journey is complete.
+Particle.prototype.update = function(index) {
+    // Remove the oldest trail particle.
+    this.trail.pop();
+    // Add the current position to the start of trail.
+    this.trail.unshift([this.x, this.y]);
+
+    // Decrease speed based on friction rate.
+    this.speed *= this.friction;
+    // Calculate current position based on angle, speed, and gravity (for y-axis only).
+    this.x += Math.cos(this.angle) * this.speed;
+    this.y += Math.sin(this.angle) * this.speed + this.gravity;
+
+    // Apply transparency based on decay.
+    this.transparency -= this.decay;
+    // Use decay rate to determine if particle should be destroyed.
+    if(this.transparency <= this.decay) {
+        // Destroy particle once transparency level is below decay.
+        particles.splice(index, 1);
+    }
+}
+
+// Draw a particle.
+// Use CanvasRenderingContext2D methods to create strokes as particle paths.
+Particle.prototype.draw = function() {
+    // Begin a new path for particle trail.
+    context.beginPath();
+    // Get the coordinates for the oldest trail position.
+    let trailEndX = this.trail[this.trail.length - 1][0];
+    let trailEndY = this.trail[this.trail.length - 1][1];
+    // Create a trail stroke from trail end position to current particle position.
+    context.moveTo(trailEndX, trailEndY);
+    context.lineTo(this.x, this.y);
+    // Set stroke coloration and style.
+    // Use hue, brightness, and transparency instead of RGBA.
+    context.strokeStyle = `hsla(${this.hue}, 100%, ${this.brightness}%, ${this.transparency})`;
+    context.stroke();
+}
+
+// === END PROTOTYPING ===
